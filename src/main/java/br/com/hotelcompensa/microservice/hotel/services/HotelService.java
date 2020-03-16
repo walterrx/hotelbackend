727a242b4b44af301f34c08763cdca6522e3af7e
walterrx <walterrx@gmail.com>
2020-03-16 19:18:42 -0300

    mocar dados do get para retorno

--- src/main/java/br/com/hotelcompensa/microservice/hotel/services/HotelService.java
+++ src/main/java/br/com/hotelcompensa/microservice/hotel/services/HotelService.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,9 +22,40 @@ private HotelRepository hotelRepository;
         this.hotelRepository = hotelRepository;
     }
 
-    @Cacheable(cacheNames = HotelDomain.CACHE_NAME, key="#hoteis")
+    //@Cacheable(cacheNames = HotelDomain.CACHE_NAME, key="#hoteis")
     public List<HotelDomain> findAll() {
-        return hotelRepository.findAll();
+
+        List<HotelDomain> retorno = new ArrayList<>();
+
+        HotelDomain hotelDomain = new HotelDomain();
+        hotelDomain.setNome("Giannela");
+        hotelDomain.setCep("00000-000");
+        hotelDomain.setCidade("Sao Paulo");
+        hotelDomain.setEndereco("Rua 1234");
+        hotelDomain.setPais("Brasil");
+
+        HotelDomain hotelDomain2 = new HotelDomain();
+        hotelDomain2.setNome("Walter");
+        hotelDomain2.setCep("00000-001");
+        hotelDomain2.setCidade("Sao Paulo");
+        hotelDomain2.setEndereco("Rua 12345");
+        hotelDomain2.setPais("Italia");
+
+
+        HotelDomain hotelDomain3 = new HotelDomain();
+        hotelDomain3.setNome("Menotti");
+        hotelDomain3.setCep("00000-002");
+        hotelDomain3.setCidade("Sao Paulo");
+        hotelDomain3.setEndereco("Rua 123456");
+        hotelDomain3.setPais("Argentina");
+
+
+        retorno.add(hotelDomain);
+        retorno.add(hotelDomain2);
+        retorno.add(hotelDomain3);
+       // return hotelRepository.findAll();
+
+        return retorno;
     }
 
 
@@ -32,12 +64,12 @@ private HotelRepository hotelRepository;
                 .orElseThrow(() -> new EntityNotFoundException("ID: " + id + " de Hotel não foi encontrado !"));
     }
 
-    @CacheEvict(cacheNames = HotelDomain.CACHE_NAME, allEntries = true)
+    //@CacheEvict(cacheNames = HotelDomain.CACHE_NAME, allEntries = true)
     public HotelDomain create(final HotelDomain hotel) {
         return hotelRepository.save(hotel);
     }
 
-    @CachePut(cacheNames = HotelDomain.CACHE_NAME, key="#hotel.getNome()")
+    //@CachePut(cacheNames = HotelDomain.CACHE_NAME, key="#hotel.getNome()")
     public HotelDomain update(final HotelDomain hotel) {
         if(hotel.getNome()== null) {
             throw new EntityNotFoundException("Hotel não encontrado !");
@@ -46,7 +78,7 @@ private HotelRepository hotelRepository;
         return hotelRepository.save(hotel);
     }
 
-    @CacheEvict(cacheNames = HotelDomain.CACHE_NAME, key="#hotel.id")
+    //@CacheEvict(cacheNames = HotelDomain.CACHE_NAME, key="#hotel.id")
     public void delete(final String id) {
         if(id == null) {
             throw new EntityNotFoundException("Hotel não encontrado !");
